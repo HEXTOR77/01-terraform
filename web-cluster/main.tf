fd7b33efe4b2bd28114c18164c995aa08941db88
HEXTOR <cys9340zx@gmail.com>
2022-12-30 16:13:00 +0900

    20221230

--- web-cluster/main.tf
+++ web-cluster/main.tf
@@ -29,10 +29,13 @@ resource "aws_launch_template" "example" {
 
 resource "aws_autoscaling_group" "example" {
   availability_zones = ["ap-northeast-2a", "ap-northeast-2c"]
-	name             = "std03-terraform-asg-example"
-  desired_capacity = 1
-  min_size         = 1
-  max_size         = 2
+  name               = "std03-terraform-asg-example"
+  desired_capacity   = 1
+  min_size           = 1
+  max_size           = 2
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   launch_template {
     id      = aws_launch_template.example.id
@@ -47,6 +50,61 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+resource "aws_lb" "example" {
+  name              = "std03-terraform-asg-example"
+  load_balancer_type = "application"
+  subnets           = data.aws_subnets.default.ids
+  security_groups   = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "std03-terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
 resource "aws_security_group" "instance" {
   name = var.security_group_name
 
@@ -54,6 +112,26 @@ resource "aws_security_group" "instance" {
     from_port   = var.server_port
     to_port     = var.server_port
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+resource "aws_security_group" "alb" {
+  name = "std03-terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -72,6 +150,6 @@ data "aws_subnets" "default" {
 data "template_file" "web_output" {
   template = file("${path.module}/web.sh")
   vars = {
-    server_port = var.server_port
+    server_port = "${var.server_port}"
   }
 }
